35b9dc635927b309c0eed5e01dc2575a667aca64
triplewz <844158830@qq.com>
2020-12-24 16:20:18 +0800

    test asm

---
+++ elliptic/sm2curve/sm2curve_test.go
@@ -0,0 +1,19 @@
+package sm2curve
+
+import "testing"
+
+func TestP256Point(t *testing.T)  {
+	Test_p256Func()
+}
+
+func TestP256InternalFunc(t *testing.T)  {
+	Test_p256InternalFunc()
+}
+
+func TestP256Func(t *testing.T)  {
+	Test_p256Func()
+}
+
+func TestArch64(t *testing.T)  {
+	Test_arch64()
+}
